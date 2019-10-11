af966f7fba7223f32f347b71011d62555d9c31e3
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2019-10-10 22:22:21 -0500

    Site Metadata and Manifest

--- garoyeri-dev-site/gatsby-config.js
+++ garoyeri-dev-site/gatsby-config.js
@@ -1,3 +1,42 @@
 module.exports = {
-  plugins: [{ resolve: `gatsby-garoyeri-dev-theme`, options: {} }],
+  siteMetadata: {
+    title: `garoyeri.dev`,
+    author: `Garo Yeriazarian`,
+    description: `Software Whisperer who finds better ways to solve software problems with people and people problems with software.`,
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/garoyeri`
+      },
+      {
+        name: `GitHub`,
+        url: `https://github.com/garoyeri`
+      },
+      {
+        name: `LinkedIn`,
+        url: `https://linkedin.com/in/garoyeri`
+      }
+    ]
+  },
+  plugins: [
+    {
+      resolve: `gatsby-garoyeri-dev-theme`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `garoyeri.dev`,
+        short_name: `garoyeri`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#FFF`,
+        display: `standalone`,
+        icon: `content/assets/icon.png`,
+        icon_options: {
+          purpose: `maskable`,
+        },
+      },
+    },
+  ],
 }
