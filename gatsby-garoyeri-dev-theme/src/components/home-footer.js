2f269e9527dc6addd21a07bbb772fcfdf2fd1881
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2019-10-10 22:42:16 -0500

    Clean up footer.

--- gatsby-garoyeri-dev-theme/src/components/home-footer.js
+++ gatsby-garoyeri-dev-theme/src/components/home-footer.js
@@ -1,29 +1,36 @@
 import React, { Fragment } from "react"
 import { Styled, css } from "theme-ui"
+import { useSiteMetadata } from "../hooks/use-site-metadata"
 
-const Footer = ({ socialLinks }) => (
-  <footer
-    css={css({
-      mt: 4,
-      pt: 3,
-    })}
-  >
-    © {new Date().getFullYear()}, Powered by
-    {` `}
-    <a href="https://www.gatsbyjs.org">Gatsby</a>
-    {` `}&bull;{` `}
-    {socialLinks.map((platform, i, arr) => (
-      <Fragment key={platform.url}>
-        <Styled.a href={platform.url} target="_blank" rel="noopener noreferrer">
-          {platform.name}
-        </Styled.a>
-        {arr.length - 1 !== i && (
-          <Fragment>
-            {` `}&bull;{` `}
-          </Fragment>
-        )}
-      </Fragment>
-    ))}
-  </footer>
-)
+const Footer = ({ socialLinks }) => {
+  const { author } = useSiteMetadata()
+
+  return (
+    <footer
+      css={css({
+        mt: 4,
+        pt: 3,
+      })}
+    >
+      © {new Date().getFullYear()} {author}
+      {` `}&bull;{` `}
+      {socialLinks.map((platform, i, arr) => (
+        <Fragment key={platform.url}>
+          <Styled.a
+            href={platform.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {platform.name}
+          </Styled.a>
+          {arr.length - 1 !== i && (
+            <Fragment>
+              {` `}&bull;{` `}
+            </Fragment>
+          )}
+        </Fragment>
+      ))}
+    </footer>
+  )
+}
 export default Footer
