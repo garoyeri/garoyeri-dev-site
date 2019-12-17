1f597b8264858b32cc4c453f4d39645ddbf37244
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2019-12-17 11:21:43 -0600

    Add extra language support to Prism blocks

--- gatsby-garoyeri-dev-theme/src/gatsby-plugin-theme-ui/components.js
+++ gatsby-garoyeri-dev-theme/src/gatsby-plugin-theme-ui/components.js
@@ -6,6 +6,10 @@ import PrismCore from "prismjs/components/prism-core"
 import "prismjs/components/prism-clike"
 import "prismjs/components/prism-csharp"
 import "prismjs/components/prism-powershell"
+import "prismjs/components/prism-javascript"
+import "prismjs/components/prism-markup"
+import "prismjs/components/prism-jsx"
+import "prismjs/components/prism-yaml"
 
 import headings from "../components/headings"
 
