379054addc65cb4732a6b3332513134c132d291a
Garo Yeriazarian <garo.yeriazarian@headspring.com>
2021-10-28 23:25:29 -0500

    Fix blog link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,10 +76,6 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
                 href: 'https://github.com/garoyeri',
               },
