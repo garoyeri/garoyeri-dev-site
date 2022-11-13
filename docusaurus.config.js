d8adc9f1c65ab0e55f4bc11ce42ef479125b5b84
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2022-11-13 00:00:08 -0600

    Upgrade deps, add new gtag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,6 +34,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-7MP5SQJPWM',
+          anonymizeIP: true,
+        }
       }),
     ],
   ],
